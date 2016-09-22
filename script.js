8c3b1e217ee4d4c9d07a8448e94059448eb3ba16
zathershtein <hostel@ukr.net>
2016-09-22 17:58:47 +0300

    it is work

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 "use strict";
 
+let Enemies = [
+	{ name: "Octopus", health: 100 },
+	{ name: "Dino", health: 170 },
+	{ name: "Ghost", health: 250 }];
+
 let enemyHTML = document.getElementById("enemy");
+const Damage = 10;
 let i = 0;
-
-const startGame = () => {
-	enemyHTML.innerHTML = '<div class="enemy-attributes"><div id="name" class="enemy-name">' + Enemies[i].name + '</div><div id="health" class="enemy-health">' + Enemies[i].health + '</div></div><div id="enemy" class="enemy-img"><img src="./img/enemy'+i+'.png" onclick="addEvent()"></div>';
-};
-
-const changeMonster = () => {
-	enemyHTML.innerHTML = '<div class="enemy-attributes"><div id="name" class="enemy-name">' + Enemies[i+1].name + '</div><div id="health" class="enemy-health">' + Enemies[i+1].health + '</div></div><div id="enemy" class="enemy-img"><img src="./img/enemy'+(i+1)+'.png" onclick="addEvent()"></div>';
-	i++;
-};
 
 const doDamage = (enemy) => {
 	enemyHTML.innerHTML += '<div id="tempFlash" style="position: fixed; top:' + (event.clientY - 25) + 'px; left:' + (event.clientX - 25) + 'px;"><img src="./img/flash1.png"></div>';
@@ -18,24 +15,25 @@ const doDamage = (enemy) => {
 
 	if (i < Enemies.length) {
 		if (enemy.health > 0) {
-			console.log(enemy.health);
-			enemy.health -= 50;	
+			enemy.health -= Damage;
+			document.getElementById("health").innerHTML = '<div id="health">' + enemy.health + '</div>';
 		}
 		else {
-			changeMonster();
+			console.log("__STOP__");
+			i++;
+			startGame();
 		};
 	}
 	else {
-		document.body.innerHTML = '<h1>The end.</h1>'
+		document.body.innerHTML = '<h1 class="end-label">The end</h1><a href="" class="end-label">Start game again!</a>'
 	};
-	
 };
 
-const addEvent = () => {
-	enemyHTML.addEventListener("click", doDamage(Enemies[i]));
+const startGame = () => {
+	console.log("-------------Game started-----------");
+	enemyHTML.innerHTML = '<div class="enemy-attributes"><div id="name" class="enemy-name">' + Enemies[i].name + '</div><div id="health" class="enemy-health">' + Enemies[i].health + '</div></div><div id="enemy" class="enemy-img"><img src="./img/enemy'+i+'.png"></div>';
+	enemyHTML.removeEventListener("click", startGame);
+	enemyHTML.addEventListener("click", () => { doDamage(Enemies[i]) });
 };
 
-let Enemies = [
-	{ name: "Octopus", health: 100 },
-	{ name: "Dino", health: 150 },
-	{ name: "Ghost", health: 230 }];
+enemyHTML.addEventListener("click", startGame);
